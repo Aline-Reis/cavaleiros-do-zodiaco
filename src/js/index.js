d07ad0f93be08b90bbb3545a09b0cf1e85fabfe7
Aline-Reis <alinnereyss@gmail.com>
2023-10-05 19:12:39 -0300

    js

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,33 @@
         passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou
         passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele 
 
-	OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem
+    OBJETIVO 2 - quando clicar no botão do personagem mostrar as informações do personagem
         passo 1 - pegar os personagens no JS pra poder mostrar ou esconder ele
         passo 2 - adicionar a classe "selecionado" no personagem que o usuário selecionou
         passo 3 - verificar se já existe um personagem selecionado, se sim, devemos remover a seleção dele 
 */
+
+const botoes = document.querySelectorAll(".botao");
+
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+});
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
